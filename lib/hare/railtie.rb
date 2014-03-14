1ec61c1201dba5cfeb05c0b9682b8594edfe1589
Clinton Judy <clinton@j-udy.com>
2014-03-14 12:08:58 -0400

    Bump version to 0.1.0.

--- lib/hare/railtie.rb
+++ lib/hare/railtie.rb
@@ -3,10 +3,5 @@ require 'rails'
 
 module Hare
   class Railtie < Rails::Railtie
-    generators do
-      require "generators/hare/install/install_generator"
-      require "generators/hare/install/message_generator"
-      require "generators/hare/install/subscription_generator"
-    end
   end
 end
