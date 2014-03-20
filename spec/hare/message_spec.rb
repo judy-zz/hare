2e2e079a137abf1bfd0a9d84020c50dbfdb4077c
Clinton Judy <clinton@j-udy.com>
2014-03-20 15:23:01 -0400

    More linting.
    It feels so good to let rubocop run rampant and tweak code based on its
    suggestions. Seriously, ya'll should try it sometime. Very relaxing.

--- spec/hare/message_spec.rb
+++ spec/hare/message_spec.rb
@@ -1,46 +1,46 @@
 describe Hare::Message do
-  describe ".exchange" do
-    it "sets and returns the exchange" do
+  describe '.exchange' do
+    it 'sets and returns the exchange' do
       dummy_class = Class.new(Hare::Message) do
-        exchange "test", type: :direct
+        exchange 'test', type: :direct
       end
-      expect(dummy_class.exchange.name).to eql "test"
+      expect(dummy_class.exchange.name).to eql 'test'
       expect(dummy_class.exchange.type).to eql :direct
     end
     it "returns the default exchange if exchange hasn't been set." do
       dummy_class = Class.new(Hare::Message)
-      expect(dummy_class.exchange.name).to eql ""
+      expect(dummy_class.exchange.name).to eql ''
       expect(dummy_class.exchange.type).to eql :direct
     end
   end
 
-  describe ".routing_key" do
-    it "sets and returns the routing_key variable" do
+  describe '.routing_key' do
+    it 'sets and returns the routing_key variable' do
       dummy_class = Class.new(Hare::Message) do
-        routing_key "test"
+        routing_key 'test'
       end
-      expect(dummy_class.routing_key).to eql "test"
+      expect(dummy_class.routing_key).to eql 'test'
     end
   end
 
-  describe "#send" do
-    it "raises an error if nothing is defined" do
+  describe '#send' do
+    it 'raises an error if nothing is defined' do
       dummy_class = Class.new(Hare::Message)
 
-      message = dummy_class.new("test")
-      expect{message.send}.to raise_error
+      message = dummy_class.new('test')
+      expect { message.send }.to raise_error
     end
 
-    it "sends a message to the default exchange" do
+    it 'sends a message to the default exchange' do
       dummy_class = Class.new(Hare::Message) do
-        routing_key "testkey"
+        routing_key 'testkey'
       end
 
-      message = dummy_class.new("test")
+      message = dummy_class.new('test')
       message.send
       result = nil
 
-      Hare::Server.channel.queue("testkey").subscribe do |delivery_info, properties, body|
+      Hare::Server.channel.queue('testkey').subscribe do |delivery_info, properties, body|
         result = body
       end
 
@@ -48,14 +48,14 @@ describe Hare::Message do
       expect(result).to eql('"test"')
     end
 
-    it "sends a message to a named exchange" do
+    it 'sends a message to a named exchange' do
       dummy_class = Class.new(Hare::Message) do
-        exchange "direct-test-exchange", type: :direct
+        exchange 'direct-test-exchange', type: :direct
       end
 
-      q = Hare::Server.channel.queue("")
-      q.bind("direct-test-exchange")
-      message = dummy_class.new("data")
+      q = Hare::Server.channel.queue('')
+      q.bind('direct-test-exchange')
+      message = dummy_class.new('data')
       message.send
       result = nil
 
--- spec/hare/subscription_spec.rb
+++ spec/hare/subscription_spec.rb
@@ -1,30 +1,30 @@
 describe Hare::Subscription do
-  describe ".subscribe" do
-    it "watches a queue" do
+  describe '.subscribe' do
+    it 'watches a queue' do
       result = nil
-      Hare::Subscription.subscribe queue: "subscription.test.queue" do |data|
+      Hare::Subscription.subscribe queue: 'subscription.test.queue' do |data|
         result = data[:string]
       end
       Hare::Server.channel.default_exchange.publish(
-        {string: "success"}.to_json,
-        routing_key: "subscription.test.queue"
+        { string: 'success' }.to_json,
+        routing_key: 'subscription.test.queue'
       )
 
       sleep(0.1)
-      expect(result).to eql "success"
+      expect(result).to eql 'success'
     end
 
-    it "sets up a binding to a named exchange" do
+    it 'sets up a binding to a named exchange' do
       result = nil
-      Hare::Subscription.subscribe bind: "named_exchange" do |data|
+      Hare::Subscription.subscribe bind: 'named_exchange' do |data|
         result = data[:string]
       end
-      Hare::Server.channel.direct("named_exchange").publish(
-        {string: "success"}.to_json
+      Hare::Server.channel.direct('named_exchange').publish(
+        { string: 'success' }.to_json
       )
 
       sleep(0.1)
-      expect(result).to eql "success"
+      expect(result).to eql 'success'
     end
   end
 end
