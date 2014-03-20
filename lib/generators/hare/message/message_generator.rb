4193ca2e09dc3d3d52dcdbf22c16ae272f84ff14
Clinton Judy <clinton@j-udy.com>
2014-03-20 13:47:35 -0400

    Add comments. More linking.

--- lib/generators/hare/message/message_generator.rb
+++ lib/generators/hare/message/message_generator.rb
@@ -1,11 +1,15 @@
 require 'rails/generators'
 
 module Hare
+  # <tt>Hare::MessageGenerator</tt> generates a template of a message file. Change the
+  # resulting file with your intended exchange or routing_key information. For example:
+  #
+  #   rails generate message user_notification
   class MessageGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
     def create_message_file
-      messages_folder = Rails.root + "app/messages"
+      messages_folder = Rails.root + 'app/messages'
       empty_directory messages_folder
       template 'message.rb', messages_folder + "#{file_name}_message.rb"
     end
--- lib/generators/hare/subscription/subscription_generator.rb
+++ lib/generators/hare/subscription/subscription_generator.rb
@@ -1,11 +1,15 @@
 require 'rails/generators'
 
 module Hare
+  # <tt>Hare::SubscriptionGenerator</tt> generates a subscription file, for
+  # example:
+  #
+  #   rails generate subscription user_notification
   class SubscriptionGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
-    def create_message_file
-      subscriptions_folder = Rails.root + "app/subscriptions"
+    def create_subscription_file
+      subscriptions_folder = Rails.root + 'app/subscriptions'
       empty_directory subscriptions_folder
       template 'subscription.rb', subscriptions_folder + "#{file_name}_subscription.rb"
     end
