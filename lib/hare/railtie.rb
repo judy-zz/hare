65afe8b99c5d903c4696edf03815ebadc5dc7a11
Clinton Judy <clinton@j-udy.com>
2014-04-07 15:43:07 -0400

    Start hare server after spring fork.
    This makes sure we have access to the full Rails
    suite inside subscription blocks.

--- lib/hare/railtie.rb
+++ lib/hare/railtie.rb
@@ -7,15 +7,20 @@ module Hare
   # structures.
   class Railtie < Rails::Railtie
     initializer 'hare' do
-      Hare::Server.start
-      sleep(1) # Give time for server to connect.
+      Spring.after_fork do
+        Hare::Server.start
+        sleep(1) # Give time for server to connect.
 
-      eagerloads = []
-      eagerloads.concat(Dir.glob("#{Rails.root}/app/messages/**/*.rb"))
-      eagerloads.concat(Dir.glob("#{Rails.root}/app/subscriptions/**/*.rb"))
+        eagerloads = []
+        eagerloads.concat(Dir.glob("#{Rails.root}/app/messages/**/*.rb"))
+        eagerloads.concat(Dir.glob("#{Rails.root}/app/subscriptions/**/*.rb"))
 
-      # TODO: I'd love to use `require_dependency` here instead, but it's doing something weird when reloading subscriptions. We might be able to close and re-open the channel before every server request, so subscription objects don't collide (or whatever they're doing that keeps them from receiving messages.)
-      eagerloads.sort.each {|file| require file }
+        # TODO: I'd love to use `require_dependency` here instead, but it's doing something weird
+        # when reloading subscriptions. We might be able to close and re-open the channel before
+        # every server request, so subscription objects don't collide (or whatever they're doing
+        # that keeps them from receiving messages.)
+        eagerloads.sort.each { |file| require file }
+      end
     end
   end
 end
