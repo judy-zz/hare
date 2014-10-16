d13e5ed090801532ded7d631da210fd8ddcd8f93
Clinton Judy <clinton@j-udy.com>
2014-10-16 09:27:32 -0400

    Remove Spring.

--- lib/hare/railtie.rb
+++ lib/hare/railtie.rb
@@ -1,6 +1,5 @@
 require 'hare'
 require 'rails'
-require 'spring/configuration'
 
 module Hare
   # This Railtie starts a connection to RabbitMQ, and eagerly loads messages
@@ -8,26 +7,23 @@ module Hare
   # structures.
   class Railtie < Rails::Railtie
     initializer 'hare' do
-      ::Spring.after_fork do
+      Hare::Server.start
 
-        Hare::Server.start
-
-        connection_attempts = 1
-        while Hare::Server.status != 'started' && connection_attempts <= 5
-          sleep(connection_attempts) # Progressively sleep longer between connection attempts.
-          connection_attempts = connection_attempts + 1
-        end
-
-        eagerloads = []
-        eagerloads.concat(Dir.glob("#{Rails.root}/app/messages/**/*.rb"))
-        eagerloads.concat(Dir.glob("#{Rails.root}/app/subscriptions/**/*.rb"))
-
-        # TODO: I'd love to use `require_dependency` here instead, but it's doing something weird
-        # when reloading subscriptions. We might be able to close and re-open the channel before
-        # every server request, so subscription objects don't collide (or whatever they're doing
-        # that keeps them from receiving messages.)
-        eagerloads.sort.each { |file| require file }
+      connection_attempts = 1
+      while Hare::Server.status != 'started' && connection_attempts <= 5
+        sleep(connection_attempts) # Progressively sleep longer between connection attempts.
+        connection_attempts = connection_attempts + 1
       end
+
+      eagerloads = []
+      eagerloads.concat(Dir.glob("#{Rails.root}/app/messages/**/*.rb"))
+      eagerloads.concat(Dir.glob("#{Rails.root}/app/subscriptions/**/*.rb"))
+
+      # TODO: I'd love to use `require_dependency` here instead, but it's doing something weird
+      # when reloading subscriptions. We might be able to close and re-open the channel before
+      # every server request, so subscription objects don't collide (or whatever they're doing
+      # that keeps them from receiving messages.)
+      eagerloads.sort.each { |file| require file }
     end
   end
 end
